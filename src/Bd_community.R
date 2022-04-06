a8fc3582d70caa2fc9ad5502fd7e3f03c0acc276
Karolina Fucikova <karolina.fucikova@gmail.com>
2022-04-06 19:28:45 -0400

    new plot with fancy colors

--- src/Bd_community.R
+++ src/Bd_community.R
@@ -8,7 +8,7 @@ Bd <- read.csv(file = "data/Bd.csv", row.names =1)
 
 # the commands that compute dissimilarity require the data samples to be rows
 # we will transpose 
-transp_bd <- t(Bd)
+transp_bd <- as.data.frame(t(Bd))
 
 # calculate Shannon index for each site
 H <- diversity(transp_bd)
@@ -57,18 +57,36 @@ fort_species <- aufort %>%
 
 # trying to plot with coloring by amount of Bd, arbitrarily assigning categories, sort of
 # L (low) - 0-10/10 ul, M (medium) - 11-30, H (high) - >30
-fort_sites$bdabund <- c("L","M","H","L","L","H","M","H","H")
+#fort_sites$bdabund <- c("L","M","H","L","L","H","M","H","H")
+# trying to do continuous based on bdabundance
+fort_sites$Bd_abundance <- transp_bd$sperangium
+
+# colorblind palette with grey:
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# or just add after geom point line to use viridis
+#  scale_color_viridis(discrete = TRUE, option = "D") +
+library(wesanderson)
+
+# http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+# https://www.datanovia.com/en/blog/top-r-color-palettes-to-know-for-great-data-visualization/
+
 
 fortify_plot <- ggplot() +
   #geom_point(data = fort_species, aes(x = MDS1, y = MDS2)) +
   #geom_point(data = fort_sites, aes(x = MDS1, y = MDS2), size = 3, shape = 2) +
   geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
   geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +
-  geom_point(data = fort_sites, aes(x = MDS1, y = MDS2, color = bdabund),size = 7, shape = 17) +
-  geom_text(data = fort_sites, size = 7, aes(x = MDS1, y = MDS2, label = Label), nudge_x = 0.1, nudge_y = 0.1) +
-   labs(x = "MDS1",
-       y = "MDS2",
-       title = "PCoA - Algal Samples from Bd Experiment")
+  geom_point(data = fort_sites, aes(x = MDS1, y = MDS2, color = Bd_abundance),size = 7, shape = 17) +
+  #scale_colour_manual(values=wes_palette("Darjeeling1", n = 3)) +
+#  scale_color_viridis(discrete = TRUE, option = "D") +
+  geom_text(data = fort_sites, size = 6, aes(x = MDS1, y = MDS2, label = Label), nudge_x = 0.1, nudge_y = 0.1) +
+  theme(legend.key.size = unit(2, 'cm'), legend.title = element_text(size=18),
+        legend.text = element_text(size=20),
+        axis.text.x = element_text(size = 15),
+        axis.text.y = element_text(size = 15),
+        axis.title = element_text(size = 20)) + 
+     labs(x = "MDS1",
+       y = "MDS2")
 fortify_plot
 
 
