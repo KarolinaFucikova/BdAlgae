0ddf8a30f046feecdfc81b8f6b9e898ff4787aea
Karolina Fucikova <karolina.fucikova@gmail.com>
2022-08-05 11:22:00 -0400

    New analyses with the complete data set.

--- src/Bd_community.R
+++ src/Bd_community.R
@@ -1,16 +1,28 @@
 library(vegan)
 
-Bd <- read.csv(file = "data/Bd.csv", row.names =1)
-# these data have abundance/concentrations of species per 10ul
+Bd <- read.csv(file = "data/Bd_data_clean.csv", row.names =1)
+# these data have abundances of species calculated from 3 passes (transects) 
+# across each of two slides per sample
+# first column has species name, second has group (family/order) affiliation
 
-# at some point it might need to be cleaned, leave out some taxa or samples
-# clean_bd <- subset(Bd, select = -HB1967)
+# taxa can be excluded if needed
+# clean_bd <- subset(Bd, select = -speciesname)
+
+# first, combine data from species in the same groups (e.g., merge all Selenastraceae)
+Bd_grouped <- aggregate(Bd[2:30], by=list(Group=Bd$Group), FUN=sum)
+# turn groups into row names
+rownames(Bd_grouped) <- Bd_grouped[,1]
+# the first column remains as group and needs to be removed prior to analyses
+Bd_grouped[,1] <- NULL
+
+# if we wish to save the cleaned, grouped data as csv:
+write.csv(Bd_grouped,"data/Bd_data_grouped.csv", row.names = TRUE)
 
 # the commands that compute dissimilarity require the data samples to be rows
 # we will transpose 
-transp_bd <- as.data.frame(t(Bd))
+transp_bd <- as.data.frame(t(Bd_grouped))
 
-# calculate Shannon index for each site
+# calculate Shannon index for each sample
 H <- diversity(transp_bd)
 H
 
@@ -24,7 +36,7 @@ bd_hc <-hclust(eucl)
 plot(bd_hc)
 
 # trying principal coordinates analysis
-# probably don't have enough data for it but seems most appropriate according to
+# seems most appropriate according to
 # https://www.davidzeleny.net/anadat-r/doku.php/en:pcoa_nmds
 # there are a lot of zeroes, so PCA and similar analyses are not ideal
 # https://websites.pmc.ucsc.edu/~mclapham/Rtips/ordination.htm#:~:text=PCoA%20is%20a%20distance%2Dbased,for%20more%20information%20on%20distances).
@@ -37,6 +49,7 @@ plot(PCoA.bd, type = "n")
 points(PCoA.bd, display = "sites", pch = 2, col="#56B4E9", cex = 1.5)
 text(PCoA.bd, display = "sites", cex=1, col="black", adj = c(1.2,0))
 
+# the code below is in progress, contains various options for plotting
 # nicer plot with ggvegan:
 library("ggvegan")
 library("ggplot2")
@@ -55,11 +68,12 @@ fort_sites <- aufort %>%
 fort_species <- aufort %>% 
   filter(Score == "species")
 
-# trying to plot with coloring by amount of Bd, arbitrarily assigning categories, sort of
-# L (low) - 0-10/10 ul, M (medium) - 11-30, H (high) - >30
+# we can plot with coloring by amount of Bd (not available in new data), 
+# one way is to arbitrarily assign categories, e.g.
+# L (low) - 0-10/sample, M (medium) - 11-30, H (high) - >30
 #fort_sites$bdabund <- c("L","M","H","L","L","H","M","H","H")
-# trying to do continuous based on bdabundance
-fort_sites$Bd_abundance <- transp_bd$sperangium
+# or make continuous based on bdabundance
+# fort_sites$Bd_abundance <- transp_bd$sperangium
 
 # colorblind palette with grey:
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
@@ -71,23 +85,23 @@ library(wesanderson)
 # https://www.datanovia.com/en/blog/top-r-color-palettes-to-know-for-great-data-visualization/
 
 
-fortify_plot <- ggplot() +
+#fortify_plot <- ggplot() +
   #geom_point(data = fort_species, aes(x = MDS1, y = MDS2)) +
   #geom_point(data = fort_sites, aes(x = MDS1, y = MDS2), size = 3, shape = 2) +
-  geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
-  geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +
-  geom_point(data = fort_sites, aes(x = MDS1, y = MDS2, color = Bd_abundance),size = 7, shape = 17) +
+#  geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
+#  geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +
+#  geom_point(data = fort_sites, aes(x = MDS1, y = MDS2, color = Bd_abundance),size = 7, shape = 17) +
   #scale_colour_manual(values=wes_palette("Darjeeling1", n = 3)) +
 #  scale_color_viridis(discrete = TRUE, option = "D") +
-  geom_text(data = fort_sites, size = 6, aes(x = MDS1, y = MDS2, label = Label), nudge_x = 0.1, nudge_y = 0.1) +
-  theme(legend.key.size = unit(2, 'cm'), legend.title = element_text(size=18),
-        legend.text = element_text(size=20),
-        axis.text.x = element_text(size = 15),
-        axis.text.y = element_text(size = 15),
-        axis.title = element_text(size = 20)) + 
-     labs(x = "MDS1",
-       y = "MDS2")
-fortify_plot
+#  geom_text(data = fort_sites, size = 6, aes(x = MDS1, y = MDS2, label = Label), nudge_x = 0.1, nudge_y = 0.1) +
+#  theme(legend.key.size = unit(2, 'cm'), legend.title = element_text(size=18),
+#        legend.text = element_text(size=20),
+#        axis.text.x = element_text(size = 15),
+#        axis.text.y = element_text(size = 15),
+#        axis.title = element_text(size = 20)) + 
+#     labs(x = "MDS1",
+#       y = "MDS2")
+#fortify_plot
 
 
 
